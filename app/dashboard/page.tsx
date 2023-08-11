c650c08098d4fdc73822d1dbf5cf18bf8b9b7adc
kelvinyelyen <kelvinyelyen@gmail.com>
2023-08-11 20:32:02 +0000

    Added protected routing

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="flex max-h-screen flex-col items-center justify-between p-24 lg:p-40 h-[550px]">
+      <h2 className="z-10 text-7xl tracking-tighter mt-16">Dashboard</h2>
+    </div>
+  )
+}
