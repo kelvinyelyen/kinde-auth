ae3e3c23c65729d6d9e97b398080e3438359d47f
kelvinyelyen <kelvinyelyen@gmail.com>
2023-12-05 23:16:57 +0000

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         Kinde Nextjs SDK integration Starter. Building <br /> blocks for your
         Next project
       </p>
-      <div className="grid grid-cols-2 gap-4 p-4">
+      <div className="grid lg:grid-cols-2 grid-cols-1  gap-4 p-4">
         <Card>
           <CardHeader>
             <CardTitle>Kinde</CardTitle>
